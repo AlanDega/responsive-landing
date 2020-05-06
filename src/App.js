1f94824e77fe000ce106fe622fa6eba9734cc3fc
AlanDega <h-unit@protonmail.com>
2020-05-06 18:24:02 -0500

    c

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="Hero">
+        <div className="HeroGroup">
+          <h1>Aprende a desarrollar y diseñar Sitios Web</h1>
+          <p>Completa los cursos con las mejores herramientas en sistemas de diseño</p>
+        </div>
+      </div>
+    </>
   );
 }
 
